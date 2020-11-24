c9f6abd918314927c9a27ddad72d6b85015e480e
unknown <jreidmke@gmail.com>
2020-11-24 12:25:36 -0600

    completed function

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,12 @@
-function addCommas() {}
+function addCommas(num) {
+    const numArr = String(num).split('');
+    const start = !numArr.includes('.') ? numArr.length - 1 : numArr.indexOf('.') - 1;
+    for(let i = start - 2; i >= 0; i-=3) {
+        numArr.splice(i, 0, ',');
+    }
+    return num;
+}
 
+
+addCommas(-3141592.65);
 module.exports = addCommas;
